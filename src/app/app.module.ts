9d2c61b72bb7b4ca7b604895b7ed6d996cc01bbd
Janessa <s00190968@mail.itsligo.ie>
2020-06-01 16:02:18 +0300

    reinstalled nodes_modules and finished writing the auth.service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { SignInComponent } from './components/sign-in/sign-in.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
+
 import { AuthService } from './shared/auth/auth.service';
 
 @NgModule({
--- src/app/shared/auth/auth.service.ts
+++ src/app/shared/auth/auth.service.ts
@@ -1,9 +1,11 @@
 import { Injectable, NgZone } from '@angular/core';
-import {User} from '../user';
+import { User } from '../user';
 import { auth } from 'firebase/app';
 import { AngularFirestore } from '@angular/fire/firestore/firestore';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
+import { AngularFirestoreDocument } from '@angular/fire/firestore/public_api';
+import { createUrlResolverWithoutPackagePrefix } from '@angular/compiler';
 
 @Injectable({
   providedIn: 'root'
@@ -33,7 +35,68 @@ export class AuthService {
   }
 
   //sign in with email/password combo
-  SignIn(email,password){
-    return this.afAuth;//continue from here
+  SignIn(email, password) {
+    return this.afAuth.auth.signInWithEmailAndPAssword(email, password).
+      then((result) => {
+        this.ngZone.run(() => {
+          this.router.navigate(['dashboard']);
+        });
+        this.SetUserData(result.user);
+      }).catch((error) => {
+        window.alert(error.message)
+      })
   }
+  //sign up email/password
+  SignUp(email, password) {
+    return this.afAuth.auth.createUserWithEmailAndPassword(email.password).
+      then((result) => {
+        //call sendverificationmail and returns a promise
+        this.SendVerificationMail();
+        this.SetUserData(result.user);
+      }).catch((error) => {
+        window.alert(error.message)
+      })
+  }
+
+  SendVerificationMail() {
+    return this.afAuth.auth.currentUser.sendEmailVerification.
+      then(() => {
+        this.router.navigate(['verify-email-address']);
+      })
+  }
+
+  ForgotPassword(passwordResetEmail) {
+    return this.afAuth.auth.sendPasswordResetEmail(passwordResetEmail).
+      then(() => {
+        window.alert('Password reset email has been sent. Check your mail');
+      }).catch((error) => {
+        window.alert(error.message)
+      })
+  }
+
+  get isLoggedIn(): boolean {
+    const user = JSON.parse(localStorage.getItem('user'));
+    return (user !== null && user.emailVerified !== false) ? true : false;
+  }
+
+  SetUserData(user) {
+    const userRef: AngularFirestoreDocument<any> = this.afs.doc(`users/${user.uid}`);
+    const userData: User = {
+      uid: user.uid,
+      name: user.name,
+      email: user.email,
+      emailVerified: user.emailVerified,
+      roles: user.Roles
+    }
+    return userRef.set(userData, { merge: true })
+  }
+
+  //sign out
+  SignOut() {
+    return this.afAuth.auth.signOut().then(() => {
+      localStorage.removeItem('user');
+      this.router.navigate(['signIn']);
+    })
+  }
+
 }
